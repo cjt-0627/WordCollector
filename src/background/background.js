ceea8b358d78b4d92a7d02227460f553ae2d3e77
cjt <alice0979528152@gmail.com>
2026-06-21 16:55:28 +0800

    dependant volume1

--- src/background/background.js
+++ src/background/background.js
@@ -46,7 +46,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       try {
         const userWordRef = collection(db, "users", uid, "words")
         
-        // 加入 book 的判斷，確保同一個單字可以存在不同單字本中
+
         const q = query(userWordRef, where("word", "==", request.word), where("book", "==", currentBook))
         const querySnapshot = await getDocs(q)
 
